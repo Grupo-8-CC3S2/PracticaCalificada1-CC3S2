1589203d84317e1a650fe2f39b2e08d00a6ba0e7
FloresVillar <efloresv@uni.pe>
2025-09-17 03:11:48 -0500

    agregando parseo grep

--- src/dns_check.sh
+++ src/dns_check.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 set -e
 set -u
@@ -5,11 +6,22 @@ set -o pipefail
 umask 022
 
 TARGET="${TARGET:-google.com}"
-DNS_SERVER="${DNS_SERVER}"
+DNS_SERVER="${DNS_SERVER:-8.8.8.8}"
 OUT_DIR="out"
 mkdir -v -p "$OUT_DIR"
 
 echo "DNS CHECK ($TARGET con $DNS_SERVER)" > "$OUT_DIR/dns_check.txt"
 dig @"$DNS_SERVER" A "$TARGET" >> "$OUT_DIR/dns_check.txt" 2>&1
 
-echo "DNS check guardado en $OUT_DIR/dns_check.txt"
+ARCHIVO="$OUT_DIR/dns_check.txt"
+
+parseo_grep(){
+    echo "parseando"
+    if [ -f ${ARCHIVO} ]; then 
+        grep -E '([0-9]{1,3}\.){3}([0-9]{1,3}){1}' ${ARCHIVO} > ${OUT_DIR}/parseo_grep.txt;fi
+}
+ 
+parseo_grep
+ 
+
+
